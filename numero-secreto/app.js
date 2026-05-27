4069e5aae81fddcaa39f7c582b9779d760decf8a
Anderson <andersonelvis191@gmail.com>
2026-05-27 07:14:51 -0300

    Mudando corretamente o número limite

--- numero-secreto/app.js
+++ numero-secreto/app.js
@@ -21,7 +21,7 @@ function exibirTextoNaTela(tag, texto) {
 //Criação da função de exibir mensagem inicial
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', `Escolha um número entre 1 e ${numeroLimite}`);
 }
 
 //Chamada da função de exibir texto na tela
